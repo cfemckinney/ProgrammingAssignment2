fcada2a20740e65aeec520897227977edac7253e
cfemckinney <cfemckinney@gmail.com>
2017-11-07 15:58:54 -0500

    Programming Assignment 2 Submission
    Submission for cfemckinney, Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions work interactively to set and calculate the inverse
+## of a matrix.
 
-## Write a short comment describing this function
+## This function sets the inverse of the matrix to NULL so it will 
+## compute a new value, then sets the function y so that the argument
+## x = y in this environment. It then lists each get and set function
+## to be returned.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		inv = NULL
+		set = function(y) {
+			x <<- y
+			inv <<- NULL
+		}
+		get = function() x
+		setinv = function(inverse) inv <<- inverse
+		getinv = function() inv
+		list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function retrieves the inverse from the makeCacheMatrix 
+## function, and looks to see if there is a NULL value. If there is a
+## NULL, it returns the inverse of the matrix, if there is not a NULL,
+## it sets the inverse from the makeCacheMatrix function and returns
+## the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       inv = x$getinv()
+       if(!is.null(inv)){
+       	message("getting cached data")
+       	return(inv)
+       }
+       data = x$get()
+       inv = solve(data, ...)
+       x$set(inv)
+       return(inv)
 }
